cc68c9eff0805728d014642927e29200b9af5767
AdItYaSiNhG <adityasingh10072003@gmail.com>
2025-04-14 16:54:53 +0530

    16 to 19

--- RESTful_API/index.js
+++ RESTful_API/index.js
@@ -1,12 +1,43 @@
 const express = require('express');
 const fs = require('fs');
-const users = require('./MOCK_DATA (1).json');
+const mongoose = require('mongoose');
+
 const app = express();
 
 const PORT = 8000;
 
+//connection
+mongoose.connect('mongodb://127.0.0.1:27017/app-1')
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log("mongo error",err));
+//Schema
+const userSchema = new mongoose.Schema({
+    firstName:{
+        type: String,
+        required: true,
+    },
+    lastName:{
+        type: String,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    jobTitile:{
+        type: String,
+    },
+    gender:{
+        type: String,
+    },
+},{timestamps: true});   
+
+const User = mongoose.model('user', userSchema);
+
 
 app.use(express.urlencoded({extended:false}));
+//urlencoded, it parses incoming requests with urlencoded payloads. 
+// It is based on body-parser.
 
 
 
@@ -16,18 +47,23 @@ app.use(express.urlencoded({extended:false}));
 //routes
 //get '/api/users'
 
-app.get('/api/users', (req, res) => {
-    res.json(users);
+app.get('/api/users', async(req, res) => {
+    const alldbUsers = await User.find({});
+    // console.log(req.headers);
+    // res.setHeader('XmyName', 'Aditya Singh');//custom header
+    //always add X before custom header name
+    res.json(alldbUsers);
 });
 
 
 /**/
-app.get('/users', (req, res) => {
+app.get('/users', async(req, res) => {
+    const alldbUsers = await User.find({});
     const html =`
     <ul>
-        ${users.map(user => 
+        ${alldbUsers.map(user => 
             `<li>
-                ${user.first_name}
+                ${user.firstName} -  ${user.email}
             </li>`
         ).join('')}
     </ul> `;
@@ -38,29 +74,35 @@ app.get('/users', (req, res) => {
 // by id 
 app
 .route('/api/users/:id')
-    .get((req, res) => {
-        const id = Number(req.params.id);
-        const user = users.find(user => user.id == id);
+    .get(async(req, res) => {
+
+        const user = await User.findById(req.params.id);
+        if(!user){
+            return res.status(404).json({
+                status: 'failed',
+                message: 'User not found'
+            });
+        }
         res.json(user);
     })
 
-    .patch((req, res) => {
-        const id = Number(req.params.id);
-        const body = req.body;
+    .patch(async(req, res) => 
+        {
+        const user = await User.findByIdAndUpdate(req.params.id, {lastName: 'verma'});
+        // const body = req.body;
 
-        const updatedUser = users.find(el => el.id === id);
-        Object.assign(updatedUser, body);
-        fs.writeFile('./MOCK_DATA (1).json', JSON.stringify(users),(err)=>{
-            if(err)
-                return res.json({
-                    status: 'failed',
-                    message: 'User not updated'
-                });
+        // const updatedUser = users.find(el => el.id === id);
+        // Object.assign(updatedUser, body);
+        // fs.writeFile('./MOCK_DATA (1).json', JSON.stringify(users),(err)=>{
+        //     if(err)
+        //         return res.json({
+        //             status: 'failed',
+        //             message: 'User not updated'
+        //         });
             
-            console.log("User updated", updatedUser);     
-            return res.json({status: 'success', id: id});
+        //     console.log("User updated", updatedUser);     
+                return res.json({status: 'success'});
             
-        });
     
     })
 
@@ -82,8 +124,34 @@ app
     });
 
 //post
-app.post('/api/users', (req, res) => {
+app.post('/api/users', async(req, res) => {
     const body = req.body;// body is not yet defined so we use middleware
+    if(
+        !body||
+        !body.first_name||
+        !body.last_name||
+        !body.email||
+        !body.gender||
+        !body.job_title
+    )
+    {
+        return res.status(400).json({
+            status: 'failed',
+            message: 'All fields are required'
+        });
+    }
+    const result = await User.create({
+        firstName: body.first_name,
+        lastName: body.last_name,
+        email: body.email,
+        gender: body.gender,
+        jobTitile: body.job_title,
+    });
+
+    return res.status(201).json({
+        status: 'success',
+    });
+
 
 /* ye use karne pe : console.log("Body", body);
    ye output mil raha hai:
@@ -96,10 +164,11 @@ app.post('/api/users', (req, res) => {
     job_title: 'Wanderer'
 }
 */
-    users.push({...body , id: users.length+1});
-    fs.writeFile('./MOCK_DATA (1).json', JSON.stringify(users),(err,data)=>{
-        return res.json({status: 'success', id: users.length});
-    });
+//before connectiong with mongodb
+    // users.push({...body , id: users.length+1});
+    // fs.writeFile('./MOCK_DATA (1).json', JSON.stringify(users),(err,data)=>{
+    //     return res.json({status: 'success', id: users.length});
+    // });
 });
 
     
